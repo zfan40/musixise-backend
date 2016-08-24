73887da03bd9869881a3a7d336a989b68d551645
zhaowei <zhaowei.me@gmail.com>
2016-08-24 22:28:10 +0800

    release

--- src/main/java/musixise/config/SecurityConfiguration.java
+++ src/main/java/musixise/config/SecurityConfiguration.java
@@ -87,6 +87,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
             .antMatchers("/api/register").permitAll()
             .antMatchers("/api/musixisers/register").permitAll()
+            .antMatchers("/api/musixisers/uploadPic").permitAll()
             .antMatchers("/api/activate").permitAll()
             .antMatchers("/api/authenticate").permitAll()
             .antMatchers("/api/account/reset_password/init").permitAll()
