453c8719cd1942bd9dee28a8be9b999857ad10d4
zhaowei <zhaowei.me@gmail.com>
2017-09-02 23:35:06 +0800

    增加文档说明

--- src/main/java/musixise/web/rest/dto/follow/AddMyFollowDTO.java
+++ src/main/java/musixise/web/rest/dto/follow/AddMyFollowDTO.java
@@ -1,14 +1,20 @@
 package musixise.web.rest.dto.follow;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.io.Serializable;
 
 /**
  * Created by zhaowei on 16/11/19.
  */
+@ApiModel(value = "AddMyFollowDTO", description = "关注对象")
 public class AddMyFollowDTO implements Serializable {
 
+    @ApiModelProperty( value = "状态 0=关注,1=取消关注", required = true )
     private Integer status;
 
+    @ApiModelProperty( value = "被关注用户ID", required = true )
     private Long followId;
 
     public Integer getStatus() {
